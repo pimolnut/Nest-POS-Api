61e67a8800f3cbf41782a24c4b582589a71dd27e
pimolnut <64010605@kmitl.ac.th>
2024-11-03 17:33:01 +0700

    fix: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('API')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
