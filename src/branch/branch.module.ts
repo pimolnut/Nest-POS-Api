e1dc0a51efbaae98f1eeef8adfda2241325b507d
pimolnut <64010605@kmitl.ac.th>
2024-11-03 13:45:35 +0700

    fix: add typeormModule

--- src/branch/branch.module.ts
+++ src/branch/branch.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BranchService } from './branch.service';
 import { BranchController } from './branch.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Branch } from './entities/branch.entity/branch.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Branch]), // Registers the repository
+  ],
   providers: [BranchService],
   controllers: [BranchController],
 })
